f811e09508d438a1cd9fc9d6e5362fdad1d73552
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-04-19 21:06:33 +0400

    Add deleting arrows on collisions.

---
+++ Assets/ArrowBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ArrowBehaviourScript : MonoBehaviour {
+	void Update() {
+		if (transform.position.y < 0) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Destroy(gameObject);
+	}
+}
